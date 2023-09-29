aee14c35f2d50e7a8ed2562883ce49d39399452d
kjlisby <16364796+kjlisby@users.noreply.github.com>
2023-09-29 20:38:35 +0200

    Create Calibrator.h

---
+++ Calibrator.h
@@ -0,0 +1,24 @@
+#ifndef CALIBRATOR_H
+#define CALIBRATOR_H
+
+#include <stdint.h>
+
+class Calibrator {
+	public:
+		void SetOffset(bool Left, bool Upwards, int Level, long NewValue);
+    void GetOffset(bool Left, bool Upwards, int Level, long NewValue);
+		
+	private:
+    String  FileName = "CalibrationData.txt";
+    void    ReadFromSD();
+    void    WriteToSD();
+    long    steps_per_level = 10000;
+    long    offset[6][2][2] = { { {0,0}, {0,0} },
+                                { {0,0}, {0,0} },
+                                { {0,0}, {0,0} },
+                                { {0,0}, {0,0} },
+                                { {0,0}, {0,0} },
+                                { {0,0}, {0,0} } }
+};
+
+#endif
