8c278f2957efae68bf271a37f2bad0346ccecbf5
kjlisby <16364796+kjlisby@users.noreply.github.com>
2023-09-29 20:41:46 +0200

    Update Calibrator.h

--- Calibrator.h
+++ Calibrator.h
@@ -4,21 +4,20 @@
 #include <stdint.h>
 
 class Calibrator {
-	public:
-		void SetOffset(bool Left, bool Upwards, int Level, long NewValue);
-    void GetOffset(bool Left, bool Upwards, int Level, long NewValue);
-		
-	private:
-    String  FileName = "CalibrationData.txt";
-    void    ReadFromSD();
-    void    WriteToSD();
-    long    steps_per_level = 10000;
-    long    offset[6][2][2] = { { {0,0}, {0,0} },
-                                { {0,0}, {0,0} },
-                                { {0,0}, {0,0} },
-                                { {0,0}, {0,0} },
-                                { {0,0}, {0,0} },
-                                { {0,0}, {0,0} } }
+        public:
+                void SetOffset(bool Left, bool Upwards, int Level, long NewValue);
+                void GetOffset(bool Left, bool Upwards, int Level, long NewValue);
+        private:
+                String  FileName = "CalibrationData.txt";
+                void    ReadFromSD();
+                void    WriteToSD();
+                long    steps_per_level = 10000;
+                long    offset[6][2][2] = { { {0,0}, {0,0} },
+                                            { {0,0}, {0,0} },
+                                            { {0,0}, {0,0} },
+                                            { {0,0}, {0,0} },
+                                            { {0,0}, {0,0} },
+                                            { {0,0}, {0,0} } };
 };
 
 #endif
