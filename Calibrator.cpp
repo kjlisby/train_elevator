ec94ea89a07aae7a3d15c78215298a124978d25d
kjlisby <16364796+kjlisby@users.noreply.github.com>
2023-09-30 20:37:59 +0200

    Create Calibrator.cpp

---
+++ Calibrator.cpp
@@ -0,0 +1,13 @@
+#include "Calibrator.h"
+
+void Calibrator::SetOffset (int Level, long NewLeftValue, long NewRightValue) {
+}
+
+long Calibrator::GetOffset (bool Left, int Level) {
+}
+
+void Calibrator::ReadFromSD () {
+}
+
+void Calibratorm::WriteToSD () {
+}
