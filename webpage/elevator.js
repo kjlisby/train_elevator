d8031d4b37bd73ebf262ee6ac98b5a4f04f1d2dc
kjlisby <16364796+kjlisby@users.noreply.github.com>
2023-10-09 13:38:40 +0200

    Full implementation - untested

--- webpage/elevator.js
+++ webpage/elevator.js
@@ -11,6 +11,7 @@ function InitWS() {
 	document.getElementById("logConsole").value += 'Listening on ws://' + window.location.hostname + ':81/';
 	Socket.onmessage = function(evt) {
 		document.getElementById("logConsole").value += evt.data;
+		UpdatePage(evt.data);
 	}
 }
 
@@ -73,7 +74,7 @@ function MoveElevatorDuringCalibration(level, leftValue, rightValue) {
 			UpdatePage(this.responseText);
 		}
 	};
-	xhttp.open("GET", "ajax_set_calibrationlevel_"+level+"_"+leftValue+"_"+rightValue, true);
+	xhttp.open("GET", "ajax_set_levelfromcalibration_"+level+"_"+leftValue+"_"+rightValue, true);
 	xhttp.send();
 }
 
@@ -90,142 +91,60 @@ function GetStatus() {
 	setTimeout('GetStatus()',1000);
 }
 
+var i CurrentLevel = 0;
 // Handle events as well as responses to various commands
 function UpdatePage(jsonString) {
 	var i;
 	var obj_list = JSON.parse(jsonString);
 	for (i in obj_list.objects) {
 		if (obj_list.objects[i].item == "STATUS") {
-		} else if (obj_list.objects[i].item == "CALBB_STORED") {
+			display = document.getElementById("OLED_copy");
+			if (obj_list.objects[i].value == "BLOCKED") {
+				display.style.font-size="5.5vw";
+				display.value="&#13;&#10;&#13;&#10;Blocked&#13;&#10;at "+CurrentLevel;
+			} else if (obj_list.objects[i].value == "HOMING1") {
+				CurrentLevel = 0;
+				display.style.font-size="5.5vw";
+				display.value="&#13;&#10;&#13;&#10;Homing&#13;&#10;phase 1";
+			} else if (obj_list.objects[i].value == "HOMING2") {
+				CurrentLevel = 0;
+				display.style.font-size="5.5vw";
+				display.value="&#13;&#10;&#13;&#10;Homing&#13;&#10;phase 2";
+			} else if (obj_list.objects[i].value == "HOMING3") {
+				CurrentLevel = 0;
+				display.style.font-size="5.5vw";
+				display.value="&#13;&#10;&#13;&#10;Homing&#13;&#10;phase 3";
+			} else if (obj_list.objects[i].value == "HOMING4") {
+				CurrentLevel = 0;
+				display.style.font-size="5.5vw";
+				display.value="&#13;&#10;&#13;&#10;Homing&#13;&#10;phase 4";
+			} else if (obj_list.objects[i].value == "MOVING") {
+				CurrentLevel = 0;
+				display.style.font-size="5.5vw";
+				var moveFrom = obj_list.objects[i+1].value;
+				var moveTo   = obj_list.objects[i+2].value;
+				if (MoveTo > moveFrom) {
+					display.value=MoveFrom+"&#13;&#10;&#8648;&#13;&#10;"+MoveTo;
+				} else {
+					display.value=MoveFrom+"&#13;&#10;&#8650;&#13;&#10;"+MoveTo;
+				}
+			} else if (obj_list.objects[i].value == "IDLE") {
+				display.style.font-size="8.5vw";
+				display.value=obj_list.objects[i+1].value;
+				if (obj_list.objects[i+1].value != CurrentLevel) {
+					CurrentLevel = obj_list.objects[i+1].value;
+					EnableCalibration(CurrentLevel);
+				}
+			} else {
+				// unknown status
+			}
+		} else if (obj_list.objects[i].item == "CAL_LEVEL") {
+			if (obj_list.objects[i+1].value == CurrentLevel) {
+				document.getElementById("cali_left_"+CurrentLevel).value = obj_list.objects[i+2].value;
+				document.getElementById("cali_rightt_"+CurrentLevel).value = obj_list.objects[i+3].value;
+			}
+		} else {
+			// unknown command
 		}
 	} /* list iteration */
 } /* function UpdatePage */
-
-
-
-/* 
-function flip(idToFlip, wanted_rotation) {
-	document.getElementById(idToFlip).style.transform = "rotate("+wanted_rotation+"deg)";
-}
-
-function warm(PS_class) {
-  var x = document.getElementsByClassName(PS_class);
-  var i;
-  for (i = 0; i < x.length; i++) {
-    x[i].src = "skinne_varm.gif";
-  }
-}
-
-function cold(PS_class) {
-  var x = document.getElementsByClassName(PS_class);
-  var i;
-  for (i = 0; i < x.length; i++) {
-    x[i].src = "skinne_kold.jpg";
-  }
-}
-
-function setPower(psu, value) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      UpdatePage(this.responseText);
-    }
-  };
-  xhttp.open("GET", "ajax_set_"+psu+"_"+value, true);
-  xhttp.send();
-}
-var psu1_slider = document.getElementById("Power1");
-psu1_slider.oninput = function() {
-  setPower("psu1", this.value);
-}
-var psu2_slider = document.getElementById("Power2");
-psu2_slider.oninput = function() {
-  setPower("psu2", this.value);
-}
-
-var x = document.getElementsByClassName("klikbar");
-var i;
-for (i = 0; i < x.length; i++) {
-  x[i].onclick = function() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        UpdatePage(this.responseText);
-      }
-  };
-  xhttp.open("GET", "ajax_set_"+this.id, true);
-  xhttp.send();
-  }
-}
-
-var output = document.getElementById("demo"); 
-function UpdatePage(jsonString) {
-   output.innerHTML = "";
-  //output.innerHTML = jsonString;
-  var i;
-  var obj_list = JSON.parse(jsonString);
-  for (i in obj_list.objects) {
-    //output.innerHTML += " "+obj_list.objects[i].item+":"+obj_list.objects[i].value;
-    if (obj_list.objects[i].item == "automode") {
-      var autobox       = document.getElementById("automode");
-      var automodelabel = document.getElementById("automodelabel");
-      if (obj_list.objects[i].value == "1") {
-        automodelabel.style.color = "red";
-        autobox.checked = true;
-      } else {
-        automodelabel.style.color = "black";
-        autobox.checked = false;
-      }
-    } else if (obj_list.objects[i].item == "PSU1FB") {
-      output.innerHTML += " "+obj_list.objects[i].item+":"+obj_list.objects[i].value;
-    } else if (obj_list.objects[i].item == "PSU2FB") {
-      output.innerHTML += " "+obj_list.objects[i].item+":"+obj_list.objects[i].value;
-    } else if (obj_list.objects[i].item == "TD1") {
-      output.innerHTML += " "+obj_list.objects[i].item+":"+obj_list.objects[i].value;
-    } else if (obj_list.objects[i].item == "TD2") {
-      output.innerHTML += " "+obj_list.objects[i].item+":"+obj_list.objects[i].value;
-    } else if (obj_list.objects[i].item == "Power1") {
-      psu1_slider.value = obj_list.objects[i].value;
-      if (obj_list.objects[i].value >= 0) {
-        flip("B4", 0);
-      } else {
-        flip("B4", 180);
-      }
-      if (obj_list.objects[i].value == 0) {
-        cold("PS1");
-      } else {
-        warm("PS1");
-      }
-    } else if (obj_list.objects[i].item == "Power2") {
-      psu2_slider.value = obj_list.objects[i].value;
-      if (obj_list.objects[i].value >= 0) {
-        flip("B7", 0);
-      } else {
-        flip("B7", 180);
-      }
-      if (obj_list.objects[i].value == 0) {
-        cold("PS2");
-      } else {
-        warm("PS2");
-      }
-    } else if (obj_list.objects[i].item.charAt(0) == 'T' && obj_list.objects[i].item.length == 2) {
-      if (obj_list.objects[i].value == "closed") {
-        document.getElementById("T"+obj_list.objects[i].item.charAt(1)+"_S").style.visibility="visible";
-        document.getElementById("T"+obj_list.objects[i].item.charAt(1)+"_T").style.visibility="hidden";
-      } else {
-        document.getElementById("T"+obj_list.objects[i].item.charAt(1)+"_S").style.visibility="hidden";
-        document.getElementById("T"+obj_list.objects[i].item.charAt(1)+"_T").style.visibility="visible";
-      }
-    } else if (obj_list.objects[i].item == "train1Pos") {
-      document.getElementById(Train1Position).style.visibility="hidden";
-      Train1Position = obj_list.objects[i].value+"_loco";
-      document.getElementById(Train1Position).style.visibility="visible";
-    } else if (obj_list.objects[i].item == "train2Pos") {
-      document.getElementById(Train2Position).style.visibility="hidden";
-      Train2Position = obj_list.objects[i].value+"_loco";
-      document.getElementById(Train2Position).style.visibility="visible";
-    }
-  }
- }
- */
-
