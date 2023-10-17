309638834fcfe2dca1eb0f78ca21a2982c03e756
kjlisby <kj@lisby.dk>
2023-10-17 17:35:56 +0200

    WIP - buttons etc. added

--- serial_elevator/PythonApp/TrainElevator.py
+++ serial_elevator/PythonApp/TrainElevator.py
@@ -1,25 +1,91 @@
 from serial import *
 from tkinter import *
+from functools import partial
 
 serialPort = "COM4"
 baudRate = 19200
-ser = Serial(serialPort , baudRate, timeout=0, writeTimeout=0) #ensure non-blocking
+# ser = Serial(serialPort , baudRate, timeout=0, writeTimeout=0) #ensure non-blocking
 
 #make a TkInter Window
 root = Tk()
-root.wm_title("Reading Serial")
+root.wm_title("Train Elevator")
 
-# make a scrollbar
-scrollbar = Scrollbar(root)
+# function to update the Display
+def displayInfo(line1, line2, line3, fontSize, height, width):
+    display.config(font =("Courier", fontSize), height=height, width=width)
+    display.delete('1.0', END)
+    display.insert(END,line1)
+    display.insert(END,line2)
+    display.insert(END,line3)
+    display.tag_add("tag_name", "1.0", "end")
+
+def restoreCalibration(level):
+    log.insert(END, "Restore calibration "+str(level)+"\n")
+def moveToCalibration(level):
+    log.insert(END, "Move to calibration "+str(level)+"\n")
+def saveCalibration(level):
+    log.insert(END, "Save calibration "+str(level)+"\n")
+
+#add buttons in a frame
+buttonFrame = Frame(root)
+#buttonFrame.pack( fill = NONE, expand = False, anchor = NW)
+buttonFrame.grid(column=0, row=0, sticky=NW)
+levelButtons = []
+restoreButtons = []
+moveButtons = []
+saveButtons = []
+for level in range (12):
+    lButton = Button(buttonFrame, text ="Level "+str(level+1), command = partial (displayInfo, "",    str(level+1),     "",220,1, 3), bg='green', fg='white')
+    levelButtons.append(lButton)
+    levelButtons[level].grid(column=0, row=level, sticky=W, padx=25, pady=5,  ipadx=25, ipady=5 )
+    lButton = Button(buttonFrame, text ="Restore", command = partial (restoreCalibration, level+1), bg='green', fg='white', state=DISABLED)
+    restoreButtons.append(lButton)
+    restoreButtons[level].grid(column=1, row=level, sticky=W, padx=25, pady=5,  ipadx=25, ipady=5 )
+    lButton = Button(buttonFrame, text ="Move", command = partial (moveToCalibration, level+1), bg='green', fg='white', state=DISABLED)
+    moveButtons.append(lButton)
+    moveButtons[level].grid(column=2, row=level, sticky=W, padx=25, pady=5,  ipadx=25, ipady=5 )
+    lButton = Button(buttonFrame, text ="Save", command = partial (saveCalibration, level+1), bg='green', fg='white', state=DISABLED)
+    saveButtons.append(lButton)
+    saveButtons[level].grid(column=3, row=level, sticky=W, padx=25, pady=5,  ipadx=25, ipady=5 )
+
+levelButtons[5].config(command = partial (displayInfo, "\n\n","Homing","", 48,5,10))
+levelButtons[6].config(command = partial (displayInfo, "\n5\n","\u21C8\n","3", 48,5,10))
+levelButtons[7].config(command = partial (displayInfo, "\n7\n","\u21CA\n","12", 48,5,10))
+levelButtons[10].config(state=DISABLED)
+restoreButtons[5].config(state=NORMAL)
+moveButtons[5].config(state=NORMAL)
+saveButtons[5].config(state=NORMAL)
+
+# make a text box to image the physical display
+display = Text ( root, bg="darkblue", fg="white", font =("Courier", 48), width=10, height=5, takefocus=0)
+display.tag_configure("tag_name", justify='center')
+#display.pack( fill = NONE, expand = False, anchor = S )
+display.grid(column=1, row=0, sticky=E)
+
+#make a frame for a scrollbar and a text box to put the serial output
+logFrame = Frame(root)
+#logFrame.pack( fill = BOTH, expand = True, side=BOTTOM )
+logFrame.grid(column=0, row=1, columnspan=2, sticky=S )
+# make the scrollbar
+scrollbar = Scrollbar(logFrame)
 scrollbar.pack(side=RIGHT, fill=Y)
-
-# make a text box to put the serial output
-log = Text ( root, width=30, height=30, takefocus=0)
-log.pack()
-
+# make the text box
+log = Text ( logFrame, width=140, height=10, takefocus=0)
+log.pack( fill = BOTH, expand = True )
 # attach text box to scrollbar
 log.config(yscrollcommand=scrollbar.set)
 scrollbar.config(command=log.yview)
+
+
+
+log.insert(END,"davs du lad os skrive noget ganske forfærdeligt langt så vi kan se, hvordan det wrapper eller ekspanderer\n")
+log.insert(END,"linie 2\n")
+log.insert(END,"davs du lad os skrive noget ganske forfærdeligt langt så vi kan se, hvordan det wrapper eller ekspanderer\n")
+log.insert(END,"linie 4\n")
+
+displayInfo("",    "2",     "",144,1, 3)
+displayInfo("\n\n","Homing","", 48,5,10)
+
 
 #make our own buffer
 #useful for parsing commands
@@ -62,14 +128,14 @@ def readSerial():
 
 
 # after initializing serial, an arduino may need a bit of time to reset
-root.after(100, readSerial)
+# root.after(100, readSerial)
 
 def readStatus():
     cmd = "get status\n"
     ser.write(cmd.encode("utf-8"))
     root.after(10000, readStatus)
     
-root.after(10000, readStatus)
+# root.after(10000, readStatus)
     
 
 root.mainloop()
