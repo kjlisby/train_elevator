e2b5b6f66796924befe6657255b3aeff8717b702
kjlisby <kj@lisby.dk>
2024-09-30 17:55:30 +0200

    Indentation unified.

--- serial_elevator/Relays.h
+++ serial_elevator/Relays.h
@@ -6,11 +6,11 @@
 #define port_extender_address 0x20
 
 class Relays {
-	public:
-		void Init     ();
-		void AtLevel  (int Level);
+  public:
+    void Init     ();
+    void AtLevel  (int Level);
 
-	private:
-		int CurrentLevel = 0;
+  private:
+    int CurrentLevel = 0;
 };
 #endif
